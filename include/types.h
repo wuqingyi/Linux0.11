aec8eceb124e8e6983d86dd994f4b1f5e7bbab05
wuqingyi <254357367@qq.com>
2017-09-07 14:52:40 +0800

    restart

--- include/types.h
+++
@@ -1,7 +0,0 @@
-#ifndef _TYPES_H
-#define _TYPES_H
-typedef unsigned char  u8;
-typedef unsigned short u16;
-typedef unsigned int   u32;
-
-#endif
--- test/hello.asm
+++
@@ -1,4 +0,0 @@
-[section .data]
-global intVal, strVal
-intVal: dd 100
-strVal: db 'hello world!',0
